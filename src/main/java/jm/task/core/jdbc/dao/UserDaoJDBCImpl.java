3c15c3fa7c3ab0bac5dc2033135263e1f2a2c59d
MacDacs <Gleb3211@icloud.com>
2024-05-07 17:25:29 +0300

    1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,90 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    public Connection connection;
 
+    public UserDaoJDBCImpl() {
+        connection = Util.getConnection();
     }
 
     public void createUsersTable() {
+        String creatTable = "CREATE TABLE IF NOT EXISTS Users ( id INT AUTO_INCREMENT PRIMARY KEY, Username VARCHAR(50) NOT NULL, Lastname VARCHAR(50) NOT NULL, Age INT(3) NOT NULL )";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(creatTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     public void dropUsersTable() {
-
+        String dropTable = "DROP TABLES IF EXISTS Users";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(dropTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String addUser = "INSERT INTO Users (Username, Lastname, Age) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(addUser)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+            System.out.println("User " + name + " was successfully saved");
+        } catch (SQLException r) {
+            throw new RuntimeException(r);
+        }
     }
 
     public void removeUserById(long id) {
-
+        String removeUser = "DELETE FROM Users WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(removeUser)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("User " + id + " was successfully removed");
+        } catch (SQLException r) {
+            throw new RuntimeException(r);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String selectAllUsers = "SELECT * FROM Users";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(selectAllUsers);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("Username"));
+                user.setLastName(resultSet.getString("Lastname"));
+                user.setAge(resultSet.getByte("Age"));
+                users.add(user);
+            }
+        } catch (SQLException r) {
+            throw new RuntimeException(r);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        String cleanTable = "DELETE FROM Users";
+        try (Statement statement = connection.prepareStatement(cleanTable)) {
+            statement.executeUpdate(cleanTable);
+        } catch (SQLException r) {
+            throw new RuntimeException(r);
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Saladforever2021";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
