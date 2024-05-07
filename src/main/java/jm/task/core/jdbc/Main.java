e0a520912ce57111e348b6e52c2968439138d8a0
MacDacs <Gleb3211@icloud.com>
2024-05-08 00:53:10 +0300

    1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,7 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
-import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
 
